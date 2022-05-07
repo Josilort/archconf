91f3b05650e5f45aaa91383c3f0590cc28577980
Josilort <josia-dittmann@web.de>
2022-05-07 14:59:58 +0100

    adding the actual configs

---
+++ home/.config/awesome/autorun.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+function run {
+  if ! pgrep -f $1 ;
+  then
+    $@&
+  fi
+}
+
+run picom -b --unredir-if-possible --backend xr_glx_hybrid --vsync --use-damage --glx-no-stencil
+run ibus-daemon -d
+run setxkbmap -layout "gb"
---
+++ home/.vimrc
@@ -0,0 +1,7 @@
+call plug#begin()
+
+Plug 'arcticicestudio/nord-vim'
+
+call plug#end()
+
+colorscheme nord
